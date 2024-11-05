5fc358cec9a31b34a0afb1419dc927d2b8bcae10
Muhammed <muhammedoyebanji@gmail.com>
2024-11-05 12:11:25 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -1,31 +1,54 @@
-let mixerProjects = mixitup('.projects__container', {
+let mixerProjects = mixitup(".projects__container", {
   selectors: {
-    target: '.project__item',
+    target: ".project__item",
   },
   animation: {
     duration: 300,
   },
 });
 
+const linkWork = document.querySelectorAll(".category__btn");
 
-const linkWork = document.querySelectorAll('.category__btn');
-
-function activeWork(){
-    linkWork.forEach((a) => a.classList.remove('active-work'));
-    this.classList.add('active-work');
+function activeWork() {
+  linkWork.forEach((a) => a.classList.remove("active-work"));
+  this.classList.add("active-work");
 }
 
-linkWork.forEach((a) => a.addEventListener('click', activeWork));
+linkWork.forEach((a) => a.addEventListener("click", activeWork));
 
 var testiSwiper = new Swiper(".testimonial__container", {
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  Message = document.getElementById("message"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    Message.value === ""
+  ) {
+    contactMessage.classList.remove('color-light');
+    contactMessage.classList.add('color-dark');
+    contactMessage.textContent = 'Write all the input fields';
+  } else {
+    emailjs.sendForm('','','','')
+  }
+};
+
+contactForm.addEventListener("submit", sendEmail);
